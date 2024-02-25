e84cd572226c41c1bc291252564882827b842aba
Vishal Kumar <63842216+vi14m@users.noreply.github.com>
2024-02-25 23:13:53 +0530

    Update ApiInterface.kt

--- app/src/main/java/com/vishal_learning/sentiment/Model/ApiInterface.kt
+++ app/src/main/java/com/vishal_learning/sentiment/Model/ApiInterface.kt
@@ -1,9 +1,6 @@
 package com.vishal_learning.sentiment.Model
 
-<<<<<<< HEAD
 import retrofit2.Response
-=======
->>>>>>> 1d2de2d (Complete)
 import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
@@ -11,13 +8,6 @@ import retrofit2.http.POST
 interface SentimentApiService {
     @Headers("Authorization: Bearer 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")
     @POST("models/cardiffnlp/twitter-roberta-base-sentiment-latest")
-<<<<<<< HEAD
-    suspend fun getSentiment(@Body payload:String): List<List<Response<Api>>>?
-}
-
-=======
     suspend fun analyzeSentiment(@Body payload: Map<String, String>): List<List<SentimentResponse>>
 }
 
-
->>>>>>> 1d2de2d (Complete)
