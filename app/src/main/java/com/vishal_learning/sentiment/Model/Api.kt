869225a36cfb6614be08178a477ce8588174c445
Vishal Kumar <63842216+vi14m@users.noreply.github.com>
2024-02-25 23:12:39 +0530

    Update Api.kt

--- app/src/main/java/com/vishal_learning/sentiment/Model/Api.kt
+++ app/src/main/java/com/vishal_learning/sentiment/Model/Api.kt
@@ -1,15 +1,8 @@
 package com.vishal_learning.sentiment.Model
 
-<<<<<<< HEAD
-data class Api(
-=======
+
 data class SentimentResponse(
->>>>>>> 1d2de2d (Complete)
     val label: String,
     val score: Double
 )
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 1d2de2d (Complete)
